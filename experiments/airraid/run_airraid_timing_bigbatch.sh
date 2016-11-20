0ec5c1c73d433ceb4a7d96845752ce9746cd734e
Richard <rliaw@berkeley.edu>
2016-11-20 13:23:45 -0800

    reduced range

--- experiments/airraid/run_airraid_timing_bigbatch.sh
+++ experiments/airraid/run_airraid_timing_bigbatch.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-
-for i in {1..15}
+for n in {1..15}
 do
+   out=$(( $n % 2 ))
+   if [ $out -eq 0 ] 
+   then
     ipython experiments/airraid/trpo_airraid_bigbatch.py $i
+   else
+	echo "$n is ODD number"	
+   fi	
 done
-
--- experiments/airraid/run_airraid_timing_bigbatch_rev.sh
+++ experiments/airraid/run_airraid_timing_bigbatch_rev.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
-
-for i in {15..1}
+for n in {15..1}
 do
+   out=$(( $n % 2 ))
+   if [ $out -eq 0 ] 
+   then
     ipython experiments/airraid/trpo_airraid_bigbatch.py $i
+   else
+	echo "$n is ODD number"	
+   fi	
 done
-
