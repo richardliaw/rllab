d590598441cde0d5c2aa1740b3a574e4815d6127
Richard <rliaw@berkeley.edu>
2016-11-20 13:24:56 -0800

    typo

--- experiments/airraid/run_airraid_timing_bigbatch_rev.sh
+++ experiments/airraid/run_airraid_timing_bigbatch_rev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for n in {15..1}
+for i in {15..1}
 do
-   out=$(( $n % 2 ))
+   out=$(( $i % 2 ))
    if [ $out -eq 0 ] 
    then
     ipython experiments/airraid/trpo_airraid_bigbatch.py $i
    else
-	echo "$n is ODD number"	
+	echo "$i is ODD number"	
    fi	
 done
