34772423fb3330d7afd55b6d51cf0c25a0021732
Richard <rliaw@berkeley.edu>
2016-12-05 12:27:27 -0800

    for high usage

--- ray_experiments/wait_stragglers/trpo_airraid_wait_ray.sh
+++ ray_experiments/wait_stragglers/trpo_airraid_wait_ray.sh
@@ -4,11 +4,11 @@ for x in {1..3}
 do
 	for i in {15..1}
 	do
-	   out=$(( $i % 3 ))
+	   out=$(( $i % 4 ))
 	   if [ $out -eq 0 ] 
 	   then
-	    ipython ray_experiments/airraid/trpo_ray_cores_waiting.py $i 0 #no wait
-	    ipython ray_experiments/airraid/trpo_ray_cores_waiting.py $i 1 #no wait
+	    # ipython ray_experiments/airraid/trpo_ray_cores_waiting.py $i 0 #no wait
+	    # ipython ray_experiments/airraid/trpo_ray_cores_waiting.py $i 1 #no wait
 	    ipython ray_experiments/airraid/trpo_ray_cores_waiting.py $i 2 #no wait
 	   else
 		echo "$i is ODD number"	
