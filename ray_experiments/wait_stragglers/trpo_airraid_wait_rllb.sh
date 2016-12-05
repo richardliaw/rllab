ab40c55d68728954be794f86bc8f28cd5ce64658
Richard <rliaw@berkeley.edu>
2016-12-05 13:34:10 -0800

    codes

--- ray_experiments/wait_stragglers/trpo_airraid_wait_rllb.sh
+++ ray_experiments/wait_stragglers/trpo_airraid_wait_rllb.sh
@@ -7,9 +7,9 @@ do
 	   out=$(( $i % 4 ))
 	   if [ $out -eq 0 ] 
 	   then
-	    # ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 0 # no wait
-	    # ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 1 # wait
-	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 2 # high_usage
+	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 0 # no wait
+	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 1 # wait
+	    # ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 2 # high_usage
 	   else
 		echo "$i is ODD number"	
 	   fi	
