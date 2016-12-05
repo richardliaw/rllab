347a0fb64b6231fbedcbf053052b5d348103c7ae
Richard <rliaw@berkeley.edu>
2016-12-05 13:05:43 -0800

    code for rllab highusage

--- ray_experiments/wait_stragglers/trpo_airraid_wait_rllb.sh
+++ ray_experiments/wait_stragglers/trpo_airraid_wait_rllb.sh
@@ -4,11 +4,11 @@ for x in {1..3}
 do
 	for i in {15..1}
 	do
-	   out=$(( $i % 3 ))
+	   out=$(( $i % 4 ))
 	   if [ $out -eq 0 ] 
 	   then
-	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 0 # no wait
-	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 1 # wait
+	    # ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 0 # no wait
+	    # ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 1 # wait
 	    ipython ray_experiments/airraid/trpo_rllb_cores_waiting.py $i 2 # high_usage
 	   else
 		echo "$i is ODD number"	
