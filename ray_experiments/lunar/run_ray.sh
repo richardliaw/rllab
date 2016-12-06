94d19a2edeaa0fe166bc9c6d84126c290120fb26
Richard <rliaw@berkeley.edu>
2016-12-05 21:30:42 -0800

    scripts

---
+++ ray_experiments/lunar/run_ray.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# for x in {1..3}
+# do
+for i in {15..1}
+do
+   out=$(( $i % 4 ))
+   if [ $out -eq 0 ] 
+   then
+    ipython ray_experiments/lunar/trpo_ray.py $i 0 # no wait
+    ipython ray_experiments/lunar/trpo_ray.py $i 1 # wait
+    ipython ray_experiments/lunar/trpo_ray.py $i 2 # high_usage # should fail
+    ipython ray_experiments/lunar/trpo_ray.py $i 3 # cp 
+   else
+	echo "$i is ODD number"	
+   fi	
+done
+# done
