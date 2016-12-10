31f49e0bf665cde17f84c400681ce0c8b878e8a5
Richard <rliaw@berkeley.edu>
2016-12-09 19:59:31 -0800

    code

--- ray_experiments/lunar/run_ray.sh
+++ ray_experiments/lunar/run_ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for x in {1..2}
+for x in {1..4}
 do
   for i in {15..1}
   do
@@ -9,8 +9,8 @@ do
      then
       ipython ray_experiments/lunar/trpo_ray.py $i 0 # no wait
       ipython ray_experiments/lunar/trpo_ray.py $i 1 # wait
-      ipython ray_experiments/lunar/trpo_ray.py $i 2 # high_usage # should fail
-      ipython ray_experiments/lunar/trpo_ray.py $i 3 # cp 
+      # ipython ray_experiments/lunar/trpo_ray.py $i 2 # high_usage # should fail
+      # ipython ray_experiments/lunar/trpo_ray.py $i 3 # cp 
      else
   	echo "$i is ODD number"	
      fi	
